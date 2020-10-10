b897e1aebaa56420cbd9b3e5f27bc3a4cbd0cafd
Amir Muminov <amir_9999@mail.ru>
2020-10-11 03:29:25 +0600

    feat: added crossorigin

--- src/main/java/kz/muminov/orderservice/controller/OrderController.java
+++ src/main/java/kz/muminov/orderservice/controller/OrderController.java
@@ -3,21 +3,32 @@ package kz.muminov.orderservice.controller;
 import kz.muminov.orderservice.model.dto.OrderBillDTO;
 import kz.muminov.orderservice.model.dto.OrderDTO;
 import kz.muminov.orderservice.model.entity.Order;
+import kz.muminov.orderservice.repository.OrderRepository;
 import kz.muminov.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class OrderController {
 
     private final OrderService orderService;
+    private final OrderRepository orderRepository;
 
     private static final String ORDER = "/order";
     private static final String ORDER_PAY = ORDER + "/bill/{id}";
     private static final String ORDER_CANCEL = ORDER + "/cancel/{id}";
+    private static final String GET_ORDERS = ORDER + "/list";
+
+    @GetMapping(GET_ORDERS)
+    public ResponseEntity<List<Order>> getOrders(){
+        return new ResponseEntity<>(orderRepository.findAll(), HttpStatus.OK);
+    }
 
     @PostMapping(ORDER)
     public ResponseEntity<Order> createOrder(@RequestBody OrderDTO orderDTO){
