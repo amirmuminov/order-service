e8d6328e0c9adeae1681d6c91861d8c1fd1050e5
Amir Muminov <amir_9999@mail.ru>
2020-12-06 21:33:45 +0600

    fix: remove headers

--- src/main/java/kz/muminov/orderservice/service/OrderService.java
+++ src/main/java/kz/muminov/orderservice/service/OrderService.java
@@ -50,7 +50,6 @@ public class OrderService {
         String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
 
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Basic " + encodedCredentials);
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
